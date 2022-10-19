eccbbf57d01fa69e870fbcf33e7239c0eb3629f6
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-10-19 17:30:29 -0300

    Interation 2 - done

--- js/index.js
+++ js/index.js
@@ -9,17 +9,21 @@ const hacker2 = "Helron Franklin"; // segundo personagem (The Navigator)
 console.log(`The navigator's name is ->`, hacker2.toLowerCase()); // Method to LowerCases
 
 // Iteration 2: Conditionals
+
 console.log(`The driver's name has = ${hacker1.length} characters long`) // The driver's characters long
 console.log(`The navigator's name has = ${hacker2.length} characters long`) // The navigator's characters long
 
 if (hacker1.length > hacker2.length) {
-    console.log(`1. The driver has the longest name, it has ${hacker1.length} characters`);
+    console.log(`1. The driver has the longest name, it has ${hacker1.length} characters`); 
+    // Case the driver's has a logest name that the navigator's.
 
    if (hacker1.length < hacker2.length) 
        console.log(`2. It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
+       // Case the navigator's has a logest name that the driver's.
 
    if (hacker1.length === hacker2.length) 
       console.log(`3. Wow, you both have equally long names, both has names with ${hacker2.length} characters`);
+      // Case the navigator's and driver's have equally  names.
 }
 
 // Iteration 3: Loops
