f292d8edbd0adfa4ae22b49a3473ba7b60e3ae70
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-10-19 16:43:01 -0300

    Interation 1 - done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 // Iteration 1: Names and Input
 
-const hacker1 = "Sergio Francalino"; // primeiro personagem (The driver's)
+const hacker1 = "Sergio Francalino"; // first studant (The driver)
 
-console.log(`I'm ${nome} the driver´s and I'm ready!`); // Line if there is working in pair
-console.log(`The Driver's name is ->`, hacker1.toUpperCase()); // método para maísculas
+console.log(`I'm ${nome} = The driver´s and i'm ready!`); // Line put if there is a second worker, in pair
+console.log(`The Driver's name is ->`, hacker1.toUpperCase()); // Method to UpperCases
 
 const hacker2 = "Helron Franklin"; // segundo personagem (The Navigator)
-console.log(`The Driver's name is ->`, hacker1.toUpperCase()); // método para maísculas
+console.log(`The navigator's name is ->`, hacker2.toLowerCase()); // Method to LowerCases
 
 // Iteration 2: Conditionals
 
