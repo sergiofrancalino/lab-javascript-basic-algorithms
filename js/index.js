929e87851b273d10935e33e15f10f41d3c2c08f7
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-10-19 16:08:29 -0300

    segunda vez

--- js/index.js
+++ js/index.js
@@ -2,7 +2,11 @@ const nome = "Sergio Francalino";
 console.log(`I'm ${nome} and I'm ready!`);
 
 // Iteration 1: Names and Input
-//
+
+const hacker1 = "Sergio Francalino"; // primeiro personagem (motorista)
+
+console.log(`The Driver's name is ->`, hacker1.toUpperCase()); // método para maísculas
+
 // Iteration 2: Conditionals
 
 
