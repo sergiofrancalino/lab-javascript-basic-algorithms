b609e34e74da0c77d64ffb161bfedf17032711a2
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-10-19 16:19:57 -0300

    third saving

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
-const nome = "Sergio Francalino";
-console.log(`I'm ${nome} and I'm ready!`);
-
 // Iteration 1: Names and Input
 
-const hacker1 = "Sergio Francalino"; // primeiro personagem (motorista)
+const hacker1 = "Sergio Francalino"; // primeiro personagem (The driver's)
 
+console.log(`I'm ${nome} the driver´s and I'm ready!`); // Line if there is working in pair
+console.log(`The Driver's name is ->`, hacker1.toUpperCase()); // método para maísculas
+
+const hacker2 = "Helron Franklin"; // segundo personagem (The Navigator)
 console.log(`The Driver's name is ->`, hacker1.toUpperCase()); // método para maísculas
 
 // Iteration 2: Conditionals
